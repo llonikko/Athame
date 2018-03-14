80101cea5bb36aed1e4ce91e70a7597e57cd7201
Joe <joe@svbnet.co>
2018-03-14 22:11:25 +1300

    Update version

--- Athame/Properties/AssemblyInfo.cs
+++ Athame/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Athame")]
-[assembly: AssemblyCopyright("Copyright © svbnet 2017")]
+[assembly: AssemblyCopyright("Copyright © svbnet 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.6.1")]
-[assembly: AssemblyFileVersion("0.4.6.1")]
+[assembly: AssemblyVersion("0.4.7.0")]
+[assembly: AssemblyFileVersion("0.4.7.0")]
